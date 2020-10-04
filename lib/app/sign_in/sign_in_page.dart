04946415496c920d996d818bf8fd9411f494e93e
zachisit <zach@zachis.it>
2020-10-03 21:49:24 -0400

    updated to v1.0.328

--- lib/app/sign_in/sign_in_page.dart
+++ lib/app/sign_in/sign_in_page.dart
@@ -130,7 +130,7 @@ class SignInPage extends StatelessWidget {
                   mainAxisAlignment: MainAxisAlignment.end,
                   children: <Widget>[
                     Text(
-                      'v1.0.327',
+                      'v1.0.328',
                       textAlign: TextAlign.center,
                       style: TextStyle(
                         fontSize: 13.0,
